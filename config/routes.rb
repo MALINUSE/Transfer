4f11bcea8348031403958443671b9fccc82a5391
IMIDSAC <imidsac@hotmail.fr>
2016-02-22 23:51:15 +0000

    routes for users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'home#index'
+
+  devise_for :users, path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      password: 'secret',
+      confirmation: 'verification',
+      unlock: 'unblock',
+      sign_up: 'register'
+  }
 
   resources :operations
   resources :people_sims
